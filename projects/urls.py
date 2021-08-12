98be2cf7199aae2627a1a423adcd0ffe53d4ea3e
FarhanSE <farhanbhn2222@gmail.com>
2021-08-12 15:13:21 +0500

    templates and template inheritance

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('hello/<str:pk>', views.hello, name='hello'),
+    path('hello/', views.hello, name='hello'),
     path('bye/', views.saybye, name='bye')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def hello(request, pk) :
-    return HttpResponse('Hello there !!!' + ' ' + str(pk))
+def hello(request) :
+    return render(request, 'projects.html')
 def saybye(request):
-    return HttpResponse('See you. Have a nice day.')
+    return render(request, 'project.html')
