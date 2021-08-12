8e106d961208e900a9f61a06a616020663202c39
FarhanSE <farhanbhn2222@gmail.com>
2021-08-12 18:15:11 +0500

    Rendering Data to Templates

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+projectlist = [
+    {
+        'id': 1,
+        'title': 'Ecommerce',
+        'description': 'fully functiona ecommerce website'
+    }, 
+    {
+        'id': 2,
+        'title': 'Portfolio',
+        'description': 'This was the project where i built out my portfolio'
+    },
+    {
+        'id': 3,
+        'title': 'Social Network',
+        'description': 'Awesome open source projects'
+    }
+]
 def hello(request) :
-    return render(request, 'projects.html')
-def saybye(request):
-    return render(request, 'project.html')
+    name = "Farhan"
+    number = 10
+    return render(request, 'projects/projects.html', {"name":name, "number":number, 'project':projectlist})
+def saybye(request, pk):
+    idnumber = None
+    for i in projectlist:
+        if i['id'] == pk:
+            idnumber = i
+    return render(request, 'projects/project.html', {'project':idnumber})
